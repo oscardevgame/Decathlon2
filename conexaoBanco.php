e085751b7bcf2a9ec96d2e7a51e7f5d7c91d11b8
darkliz <darkliz@gmail.com>
2014-01-27 18:42:11 -0200

    Servers infos

--- conexaoBanco.php
+++ conexaoBanco.php
@@ -7,7 +7,7 @@
 #########################################
 
 /*
-* Classe conex�o DAO
+* Classe conexao DAO
 * -------------------------------------
 */
 
@@ -16,10 +16,38 @@ class conexaoBanco{
 	public $pdo = NULL;
 
 	public function conectar(){
+		if (strpos($_SERVER['SERVER_NAME'], 'decathlon.bl.ee') !== false) {
+			conectarRemoto;
+		}else{
+			conectarLocal;
+		}
+	}
+	
+	function conectarLocal(){
 		// Conexao com MySQL via PDO
 		$dsn = 'mysql:host=localhost;port=3306;dbname=DBDecathlon';
 		$usuario = 'root';
 		$senha = '';
+		$opcoes = array(
+			PDO::ATTR_PERSISTENT => true,
+			PDO::ATTR_CASE => PDO::CASE_LOWER,
+                        PDO::ATTR_EMULATE_PREPARES => true,
+                        PDO::ERRMODE_WARNING => true, 
+                        PDO::ERRMODE_EXCEPTION => true
+		);
+
+		try {
+			$this->pdo = new PDO($dsn, $usuario, $senha, $opcoes);
+		} catch (PDOException $e) {
+			echo 'Erro: '.$e->getMessage();
+		}
+	}
+	
+	function conectarRemoto(){
+		// Conexao com MySQL via PDO
+		$dsn = 'mysql:host=mysql.hostinger.com.br;port=3306;dbname=u893297990_deca';
+		$usuario = 'u893297990_root';
+		$senha = 'decaroot';
 		$opcoes = array(
 			PDO::ATTR_PERSISTENT => true,
 			PDO::ATTR_CASE => PDO::CASE_LOWER,
